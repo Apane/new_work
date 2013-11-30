885e4a60c64ec9eda76741dc436d5f4e00acbbe8
Apane <Anthony.panepinto101@gmail.com>
2013-11-30 18:20:31 -0500

    heroku push

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131129020946) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "photos", force: true do |t|
     t.string   "file"
     t.integer  "attachable_id"
