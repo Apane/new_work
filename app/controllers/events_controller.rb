2d33bfbe6bbb3672f65f068781f4213edbb887f3
Rosca Sergiu <rmagnum2002@gmail.com>
2013-12-05 23:48:29 +0200

    fixes for events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,39 @@
 class EventsController < ApplicationController
-  def new
-    @event = Event.new
-  end
-
-  def create
-    @event = Event.new(params[:event])
-    if @event.save
-      redirect_to :action => 'index'
-    else
-      @events = Event.find(:all)
-      render :action => 'new'
-    end
-  end
+  before_filter :authenticate_user!
 
   def index
     @user = current_user
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  # def create
+  #   @event = Event.new(params[:event])
+  #   if @event.save
+  #     redirect_to :action => 'index'
+  #   else
+  #     @events = Event.find(:all)
+  #     render :action => 'new'
+  #   end
+  # end
+
+  def create
+    @event = current_user.events.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to :back, notice: 'Event was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @event }
+        format.js
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
   end
 
   def show
@@ -39,6 +57,21 @@ class EventsController < ApplicationController
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
-    redirect_to :back
+
+    respond_to do |format|
+      format.html {redirect_to :back}
+      format.js
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, 
-                  :password, 
-                  :password_confirmation, 
-                  :zip, 
-                  :gender, 
-                  :remember_me, 
-                  :first_name, 
-                  :last_name, 
-                  :birthday, 
-                  :current_password, 
-                  :occupation, 
-                  :address, 
-                  :interests, 
-                  :aboutme, 
+  attr_accessible :email,
+                  :password,
+                  :password_confirmation,
+                  :zip,
+                  :gender,
+                  :remember_me,
+                  :first_name,
+                  :last_name,
+                  :birthday,
+                  :current_password,
+                  :occupation,
+                  :address,
+                  :interests,
+                  :aboutme,
                   :profile_image,
                   :photos_attributes
 
@@ -24,10 +24,17 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :photos
   mount_uploader :profile_image, ProfileImageUploader
 
-  validates :gender, :presence => true 
+  validates :gender, :presence => true
 
   def number_of_users
     User.all.count
   end
 
+  def name
+    self.first_name + ' ' + self.last_name
+  end
+
+  def short_name
+    self.first_name + ' ' + self.last_name[0,1] + '.'
+  end
 end
