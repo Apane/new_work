18a23a6b0e3e541042b5ebeb8c5aa27a542f271a
Apane <Anthony.panepinto101@gmail.com>
2013-12-17 13:27:55 -0500

    added errors to sign in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,12 @@
 class Users::SessionsController < Devise::SessionsController
 
-
   private
 
   def after_sign_in_path_for(resource)
     if user_signed_in?
       welcome_path
     else
+      flash[:notice] = "Something went wrong"
       new_user_registration_path
     end
   end
