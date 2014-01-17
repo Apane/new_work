f053b2c3e3e49ed9dbfdf6942fbcecefb4b4df90
Rosca Sergiu <rmagnum2002@gmail.com>
2014-01-17 16:51:34 +0200

    added secret_key_base required by rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Friendiose::Application.config.secret_token = 'f0c157e3381dcd5c2ca8b0b2f012f4ded875b40f3d86302776c179f27264bfa9b98cca6e28470dcb965d8c03b47980bb52925401c8c1a5307b47487123f4ad01'
+Friendiose::Application.config.secret_key_base = '5096a51bb81789965c9b72315138380a73b5ebc922be1a7d6d359cc7b92b6ca918fe089286c14b33f7f27f56a8cf852bc3e1c5a35560fbc84df9bcf4c430d678'
