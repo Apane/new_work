727681fe652b46b290dc906748ebe6b2bc287e46
Apane <Anthony.panepinto101@gmail.com>
2014-02-04 11:17:07 -0500

    fixed favorites model bug

--- app/models/favorite_relationship.rb
+++ app/models/favorite_relationship.rb
@@ -1,5 +1,5 @@
-class FavoriteRelationships < ActiveRecord::Base
+class FavoriteRelationship < ActiveRecord::Base
   # attributes are ( user_id and favourite_id )
-  belongs_to :favourite
+  belongs_to :favorite
   belongs_to :user
 end
