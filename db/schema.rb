3bf0834e330904b02499b15a7f7c3eefdc6702bd
Apane <Anthony.panepinto101@gmail.com>
2014-02-14 19:03:22 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 20140214150634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authorizations", force: true do |t|
     t.string   "provider"
     t.string   "uid"
@@ -129,6 +132,13 @@ ActiveRecord::Schema.define(version: 20140214150634) do
   create_table "hidden_users", force: true do |t|
     t.integer  "user_id"
     t.integer  "hidden_user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "hiddens", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "hidden_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
