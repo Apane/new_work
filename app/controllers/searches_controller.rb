c669a6dbff89dbe4bc9118b14a2530d4c525f61d
Rosca Sergiu <rmagnum2002@gmail.com>
2014-03-08 21:40:42 +0200

    search filter: display events, activities and users in 50 miles range

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,9 @@ class SearchesController < ApplicationController
 
   def index
     terms = params[:search][:terms]
-    @users = User.filtered(terms)
-    @events = Event.where('event_date >= ?', Time.now.beginning_of_day).filtered(terms)
-    @activities = Activity.where('activity_date >= ?', Time.now.beginning_of_day).filtered(terms)
+    @users = User.in_range(0..50, origin: current_user).filtered(terms)
+    @events = Event.in_range(0..50, origin: current_user).where('event_date >= ?', Time.now.beginning_of_day).filtered(terms)
+    @activities = Activity.in_range(0..50, origin: current_user).where('activity_date >= ?', Time.now.beginning_of_day).filtered(terms)
 
     respond_to do |format|
       format.html { redirect_to :back }
