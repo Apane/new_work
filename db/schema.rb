39cf9eb81336774d5215763c11ce2a2d3d599648
Apane <Anthony.panepinto101@gmail.com>
2014-03-14 17:28:00 -0400

    added admin

--- db/schema.rb
+++ db/schema.rb
@@ -194,6 +194,13 @@ ActiveRecord::Schema.define(version: 20140314150452) do
     t.datetime "updated_at"
   end
 
+  create_table "hiddens", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "hidden_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "messages", force: true do |t|
     t.integer  "conversation_id"
     t.integer  "sender_id"
