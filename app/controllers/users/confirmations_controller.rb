38a74a798feae059bcfd86d626a34c875d4abada
Apane <Anthony.panepinto101@gmail.com>
2014-04-01 16:05:31 -0400

    updated devise.rb

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -48,13 +48,5 @@ class Devise::ConfirmationsController < DeviseController
     def after_resending_confirmation_instructions_path_for(resource_name)
       new_session_path(resource_name) if is_navigational_format?
     end
-
-    # The path used after confirmation.
-    def after_confirmation_path_for(resource_name, resource)
-      if signed_in?
-        signed_in_root_path(resource)
-      else
-        new_session_path(resource_name)
-      end
     end
 end
