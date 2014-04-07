793a4f177e205446f2fda618cf136eccc998009c
Apane <Anthony.panepinto101@gmail.com>
2014-04-07 10:31:57 -0400

    updated top5

--- db/schema.rb
+++ db/schema.rb
@@ -210,6 +210,13 @@ ActiveRecord::Schema.define(version: 20140407143848) do
     t.datetime "updated_at"
   end
 
+  create_table "hiddens", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "hidden_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "mail_settings", force: true do |t|
     t.integer  "user_id"
     t.boolean  "new_message",      default: true
