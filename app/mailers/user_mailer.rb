e0bdb368b9712573a112a4bbfcb8a55c7352b149
Apane <Anthony.panepinto101@gmail.com>
2014-04-07 11:34:06 -0400

    updated email messages and subject line

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,21 +10,21 @@ class UserMailer < ActionMailer::Base
     @message = message
     @recipient = message.recipient
     @sender = message.sender
-    mail to: @recipient.email, subject: "Friendiose - new message from #{@sender.name}"
+    mail to: @recipient.email, subject: "Friendiose - #{@recipient}, You have received a new message from #{@sender.name}"
   end
 
   def new_participant(event_participant)
     @event = event_participant.event
     @owner = @event.user
     @participant = event_participant.user
-    mail to: @owner.email, subject: "Friendiose - new attendee to #{@event.title} event"
+    mail to: @owner.email, subject: "Friendiose - #{@participant}, has RSVP'd for your #{@event.title} event"
   end
 
   def new_activity_participant(participant)
     @activity = participant.activity
     @owner = @activity.user
     @participant = participant.user
-    mail to: @owner.email, subject: "Friendiose - user interested in your #{@activity.title} activity"
+    mail to: @owner.email, subject: "Friendiose - #{@participant} is interested in your #{@activity.title} activity"
   end
 
   def new_comment(comment, participant)
@@ -32,14 +32,14 @@ class UserMailer < ActionMailer::Base
     @commenter = comment.user
     @commentable = comment.commentable
     @participant = participant
-    mail to: participant.email, subject: "Friendiose - new comment to #{@commentable.title} #{@commentable.class.name.downcase}"
+    mail to: participant.email, subject: "Friendiose - New comment by #{@commenter.name} on #{@commentable.title} (#{@commentable.class.name.downcase})"
   end
 
   def new_visitor(visit)
     @user = visit.user
     @visitor = visit.visitor
 
-    mail to: @user.email, subject: "Friendiose - #{@visitor.name} visited your profile"
+    mail to: @user.email, subject: "#{@visitor.name} viewed your Friendiose profile!"
   end
 
   # def reply_to_request(request)
