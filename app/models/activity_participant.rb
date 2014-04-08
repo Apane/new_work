200b4fa8d96629188a6d3444e58a250f257de1a1
Rosca Sergiu <rmagnum2002@gmail.com>
2014-04-08 21:28:49 +0300

    changes in activities

--- app/models/activity_participant.rb
+++ app/models/activity_participant.rb
@@ -6,6 +6,9 @@ class ActivityParticipant < ActiveRecord::Base
   after_create :notify_owner
 
   def notify_owner # owner gets email when new participant joins to event
-    UserMailer.new_activity_participant(self).deliver if self.activity.user.accepts_email_for_new_participant?
+    activity = self.activity
+    if self.user_id != activity.user_id #do not send to event owner when self join after event is created
+      UserMailer.new_activity_participant(self).deliver if activity.user.accepts_email_for_new_participant?
+    end
   end
 end
