990b2cbfd0398cf040ba83c2c36392e237377f01
Rosca Sergiu <rmagnum2002@gmail.com>
2014-04-12 02:19:34 +0300

    cleaned up oauth callbacks controller

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -12,4 +12,32 @@ class Authorization < ActiveRecord::Base
     end
   end
 
+  def self.auth_social(provider, access_token)
+    if provider == "Facebook"
+      uid = access_token['uid']
+      email = access_token['info']['email']
+      auth_attr = { :uid => uid, :token => access_token['credentials']['token'],
+        :secret => nil, :first_name => access_token['info']['first_name'],
+        :last_name => access_token['info']['last_name'], :name => access_token['info']['name'],
+        :link => access_token['extra']['raw_info']['link'] }
+    elsif provider == "Twitter"
+      uid = access_token['extra']['raw_info']['id']
+      name = access_token['extra']['raw_info']['name']
+      auth_attr = { :uid => uid, :token => access_token['credentials']['token'],
+        :secret => access_token['credentials']['secret'], :first_name => access_token['info']['first_name'],
+        :last_name => access_token['info']['last_name'], :name => name,
+        :link => "http://twitter.com/#{name}", :connections_count => access_token['extra']['raw_info']['followers_count'] }
+    elsif provider == 'LinkedIn' || provider == 'GPlus'
+      uid = access_token['uid']
+      name = access_token['info']['name']
+      auth_attr = { :uid => uid, :token => access_token['credentials']['token'],
+        :secret => access_token['credentials']['secret'], :first_name => access_token['info']['first_name'],
+        :last_name => access_token['info']['last_name'],
+        :link => (provider == 'LinkedIn') ? access_token['info']['public_profile_url'] : access_token['info']['image'] }
+    else
+      raise 'Provider #{provider} not handled'
+    end
+
+    return [uid, name, auth_attr]
+  end
 end
