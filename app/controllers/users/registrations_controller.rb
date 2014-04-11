1ba9757f185a718d3b6fac993fb8b0a29097a141
Rosca Sergiu <rmagnum2002@gmail.com>
2014-04-12 00:08:11 +0300

    cleaned up registrations controller, added code to update user account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -37,20 +37,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def update
-    if params[resource_name][:password].blank?
-      params[resource_name].delete(:password)
-      params[resource_name].delete(:password_confirmation) if params[resource_name][:password_confirmation].blank?
-    end
-    # Override Devise to use update_attributes instead of update_with_password.
-    # This is the only change we make.
     if resource.update_attributes(params[resource_name])
       set_flash_message :notice, :updated
-      # Line below required if using Devise >= 1.2.0
-      sign_in resource_name, resource, :bypass => true
-      redirect_to after_update_path_for(resource)
+      sign_in resource_name, resource, :bypass => true # Line required if using Devise >= 1.2.0
+      redirect_to :back #after_update_path_for(resource)
     else
       clean_up_passwords(resource)
-      render_with_scope :edit
+      redirect_to :back, notice: 'Account was not updated!'
     end
   end
 
